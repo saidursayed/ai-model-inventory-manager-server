7ac3025f6905aeace037dcb92d72d8b007ba1437
saidurrahmansayed <mdsaidurrahmansayed194@gmail.com>
2026-02-13 23:57:40 +0600

    ai-model-inventory-manager-server

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("AI Model Inventory Manager Server");
+});
+
+const uri =
+  "mongodb+srv://ai-model-inventory-manager-db:5HaN8ztFHznoN2t1@simple-crud-server.63zgbdx.mongodb.net/?appName=simple-crud-server";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("ai-model-db");
+    const aiModelCollection = db.collection("ai-models");
+
+    // latest models
+    app.get("/latest-models", async (req, res) => {
+      const cursor = aiModelCollection
+        .find()
+        .sort({ createdAt: "desc" })
+        .limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // all models
+    app.get("/models", async (req, res) => {
+      const cursor = aiModelCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // single model
+    app.get("/models/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await aiModelCollection.findOne(query);
+      res.send(result);
+    });
+
+    // add model
+    app.post("/add-model", async (req, res) => {
+      const newModel = req.body;
+      const result = await aiModelCollection.insertOne(newModel);
+      res.send(result);
+    });
+
+    // my model
+    app.get("/my-models", async (req, res) => {
+      const email = req.query.email;
+      const cursor = aiModelCollection.find({
+        createdBy: email,
+      });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // update model
+    app.put("/models/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedModel = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: updatedModel,
+      };
+      const result = await aiModelCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    // delete model
+    app.delete("/models/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await aiModelCollection.deleteOne(query);
+      res.send(result);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`AI Model Inventory Manager Server is listening on port ${port}`);
+});
